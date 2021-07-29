1a60b9ed5c6302dab35f5ce98a63051e93a1a9c2
guoxilu <936329720@qq.com>
2021-07-29 22:04:39 +0800

    第三次修改

--- run.py
+++ run.py
@@ -15,10 +15,10 @@ def get_test_cases(dirpath):
 
 
 if __name__ == '__main__':
-    cases = get_test_cases('../testcase')
+    cases = get_test_cases('./testcase')
     now = time.strftime("%Y-%m-%d %H_%M_%S")  # 报告生成时间
-    test_reports_address = '../report'      # 测试报告存放位置
-    filename = '../report/' + now + 'report.html'  # 设置报告文件名
+    test_reports_address = './report'      # 测试报告存放位置
+    filename = './report/' + now + 'report.html'  # 设置报告文件名
     fp = open(filename, 'wb')
     runner = HTMLTestRunner.HTMLTestRunner(stream=fp, title=u'Web自动化测试', description=u'详细测试结果如下:')
     runner.run(cases)
