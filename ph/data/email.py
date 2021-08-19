0158d2a6195d8fc0dbe7df159193f9f0d02fba06
guozilubbg <60023704+guozilubbg@users.noreply.github.com>
2021-08-19 09:12:53 +0800

    Create email.py

--- ph/data/email.py
+++ ph/data/email.py
@@ -4,8 +4,8 @@
 '''
 class email():
     mail_server = "smtp.qq.com"#邮箱地址
-    send_user = "936329720@qq.com"#发件人
-    to_user = '936329720@qq.com'#收件人
+    send_user = "@qq.com"#发件人
+    to_user = '@qq.com'#收件人
     # subject = 'web自动化测试报告测试报告' #邮件标题
-    password = "eoqphgtagvxbbbfc"
-    subject = '公卫家医签约自动化测试报告' #邮件标题
+    password = ""
+    subject = '自动化测试报告' #邮件标题
