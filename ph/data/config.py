290ddcd4aebb3f7431f4e067faad996c8728192d
guozilubbg <60023704+guozilubbg@users.noreply.github.com>
2021-08-19 09:14:06 +0800

    Update config.py

--- ph/data/config.py
+++ ph/data/config.py
@@ -4,14 +4,13 @@
 
 class BaseConf(object):
     # mysql配置信息
-    MYSQL_HOST = "172.16.19.106"
-    MYSQL_PORT = "3306"
-    MYSQL_USER = "admin"
-    MYSQL_PWD = "Admin!123"
-    MYSQL_DB = 'test_br_sc_his_20210421'
+    MYSQL_HOST = ""
+    MYSQL_PORT = "
+    MYSQL_USER = ""
+    MYSQL_PWD = ""
+    MYSQL_DB = ''
 
     # url
-    public_health_url = "http://172.16.19.227/dashboard/workplace"#"http://172.16.19.203:9081/login"
 
 
 
